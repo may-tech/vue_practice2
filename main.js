c8ed0d2fff7b63000931c220f614802e921c24d5
may-tech <aehkloprsw@gmail.com>
2020-04-04 16:42:34 +0900

    [add] v-bindによるリンクの追加、スタイルの設定

--- main.js
+++ main.js
@@ -2,6 +2,7 @@ var app = new Vue({
   el: '#app',
   data: {
     product: 'Socks',
-    description: 'データが{{}}の中に出力されるよ'
+    description: 'データが{{}}の中に出力されるよ',
+    link: 'https://www.vuemastery.com/courses/intro-to-vue-js/attribute-binding'
   }
 })
