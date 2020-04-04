380580c3b79129abab124a514ec9b7780de784b1
may-tech <aehkloprsw@gmail.com>
2020-04-04 17:00:38 +0900

    [add] v-if、v-else-if、v-else、v-showの追加

--- main.js
+++ main.js
@@ -2,7 +2,8 @@ var app = new Vue({
   el: '#app',
   data: {
     product: 'Socks',
-    description: 'データが{{}}の中に出力されるよ',
-    link: 'https://www.vuemastery.com/courses/intro-to-vue-js/attribute-binding'
+    link: 'https://www.vuemastery.com/courses/intro-to-vue-js/attribute-binding',
+    inventry: '10',
+    Instock: false
   }
 })
