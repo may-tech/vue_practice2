46b0fbeb1db690ab891f6afc628c1f6c5cfa8983
may-tech <aehkloprsw@gmail.com>
2020-04-06 05:55:21 +0900

    [add] コメント、v-forによるリストの追加

--- main.js
+++ main.js
@@ -3,7 +3,18 @@ var app = new Vue({
   data: {
     product: 'Socks',
     link: 'https://www.vuemastery.com/courses/intro-to-vue-js/attribute-binding',
-    inventry: '10',
-    Instock: false
+    // inventry: '10',
+    inStock: true,
+    details: ["80% cotton", "20% polyester", "Gender-neutral"],
+    variants: [
+      {
+        variantId: 2234,
+        variantColor: "green"
+      },
+      {
+        variantId: 2235,
+        variantColor: "blue"
+      }
+    ]
   }
 })
