084d0ba5bfc172690b6ade9a40826a967d93c3e7
may-tech <aehkloprsw@gmail.com>
2020-04-07 22:10:03 +0900

    [add] 算出プロパティの追加

--- main.js
+++ main.js
@@ -1,20 +1,22 @@
 var app = new Vue({
   el: '#app',
   data: {
+    brand: 'VueMastery',
     product: 'Socks',
-    image: 'images/green-cocktail.jpeg',
-    inStock: false,
+    selectedVariant: 0,
     details: ["80% cotton", "20% polyester", "Gender-neutral"],
     variants: [
       {
         variantId: 2234,
         variantColor: 'green',
-        variantImage: 'images/green-cocktail.jpeg'
+        variantImage: 'images/green-cocktail.jpeg',
+        variantQuantity: 10
       },
       {
         variantId: 2235,
         variantColor: 'pink',
-        variantImage: 'images/pink-cocktail.jpeg'
+        variantImage: 'images/pink-cocktail.jpeg',
+        variantQuantity: 0
       }
     ],
     cart: 0
@@ -26,8 +28,24 @@ var app = new Vue({
     decrements: function(){
       this.cart -=1
     },
-    updataProduct: function(variantImage){
-      this.image = variantImage
+    updataProduct: function(index){
+      this.selectedVariant = index
+      console.log(index)
     }
+  },
+    computed: {
+      // 「brand」と「product」を足して「title」プロパティとする。
+      title(){
+        return this.brand + ' ' + this.product 
+      },
+      // 配列データ「variants[selectedVariant(0 or 1)]」の「variantImage」を「image」とする。
+      image(){
+        return this.variants[this.selectedVariant].variantImage
+      },
+      // 配列データ「variants[selectedVariant(0 or 1)]」の「variantQuantity」を「inStock」とする。
+      inStock(){
+        return this.variants[this.selectedVariant].variantQuantity
+
+      }
   }
 })
